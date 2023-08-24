bf620c3128255d35fe7a1b1bb3bbf2785cf35e2b
Karan Joshi <Karan190213@gmail.com>
2023-08-24 17:40:37 +0530

    Updated Configuration for Webpack Dev Server and README
    Modified the project's configuration files to enable smooth execution of the Webpack Dev Server.
    Additionally, made updates to the README file to provide comprehensive instructions for setting up and running the project

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   output: {
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
     clean: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-        ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-      devtool: 'inline-source-map',
+    ],
+  },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
@@ -29,11 +29,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       template: './src/template.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   optimization: {
+
+  optimization: {
     runtimeChunk: 'single',
   },
- };
+};
