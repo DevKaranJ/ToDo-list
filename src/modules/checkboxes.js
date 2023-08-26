e5339087388cba129d43bb731d353a6e5c45b547
Karan Joshi <Karan190213@gmail.com>
2023-08-26 18:39:20 +0530

    Updated The status functionality

--- src/modules/checkboxes.js
+++ src/modules/checkboxes.js
@@ -7,6 +7,13 @@ export function addCheckboxFunctionality(taskList) {
       const listItem = findParentListItem(event.target);
       if (listItem) {
         const taskText = listItem.querySelector('.task-item span');
+      
+        // Update the completed key in the local storage data
+        const tasks = JSON.parse(localStorage.getItem('tasks'));
+        const taskIndex = Array.from(taskList.querySelectorAll('li')).indexOf(listItem);
+        tasks[taskIndex].completed = event.target.checked;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
         taskText.classList.toggle('completed', event.target.checked);
 
         saveTasksToLocalStorage(taskList);
--- src/modules/taskFunctions.js
+++ src/modules/taskFunctions.js
@@ -2,10 +2,10 @@
 export function saveTasksToLocalStorage(taskList) {
   const tasks = [];
   const taskItems = taskList.querySelectorAll('li');
-  taskItems.forEach((taskItem, index) => {
-    const text = taskItem.querySelector('span').textContent;
-    const completed = false;
-    tasks.push({ index: index + 1, text, completed });
+  taskItems.forEach((taskItem) => {
+    const text = taskItem.querySelector('.task-item span').textContent;
+    const completed = taskItem.querySelector('.task-checkbox').checked;
+    tasks.push({ text, completed });
   });
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
