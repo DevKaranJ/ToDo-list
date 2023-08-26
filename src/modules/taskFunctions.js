77009cf56d259de7fd6ab50993cc045faa178d70
Karan Joshi <Karan190213@gmail.com>
2023-08-26 20:29:34 +0530

    Add and Update Task Indexes, Handle Deletion
    Implemented the necessary changes to ensure that each task is assigned a unique index. Additionally, the task indexes are now correctly updated even after task deletions. This fulfills the project's requirement for maintaining accurate task indexes throughout the task list.

--- src/modules/taskFunctions.js
+++ src/modules/taskFunctions.js
@@ -2,10 +2,10 @@
 export function saveTasksToLocalStorage(taskList) {
   const tasks = [];
   const taskItems = taskList.querySelectorAll('li');
-  taskItems.forEach((taskItem) => {
+  taskItems.forEach((taskItem, index) => {
     const text = taskItem.querySelector('.task-item span').textContent;
     const completed = taskItem.querySelector('.task-checkbox').checked;
-    tasks.push({ text, completed });
+    tasks.push({ index: index + 1, text, completed });
   });
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
